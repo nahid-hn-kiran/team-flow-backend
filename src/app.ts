0b3c6d5876c4837fd57644c39d961db7073bc8fb
nahid7g <nahid.h.kiran@gmail.com>
2026-09-07 18:13:17 +0600

    Live backend url added to cors

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use(
     origin: [
       envVars.google.FRONTEND_URL,
       envVars.BETTER_AUTH_URL,
+      "https://team-flow-backend-1.onrender.com",
       "http://localhost:3000",
       "http://localhost:5000",
     ],
